b2705aa00bbcea0f7f4ed1d0ad3ada4300687b58
Suzane Feitosa <suzanefeitosa08@icloud.com>
2023-01-20 21:55:33 -0300

    new adjustment html and css

---
+++ script.js
@@ -0,0 +1,18 @@
+// Obtém o botão que abre o modal
+var button = document.getElementById("button-trailer");
+
+// Obtém o modal
+var modal = document.getElementById("button-trailer");
+
+// Obtém o botão de fechamento do modal
+var span = document.getElementsByClassName("close")[0];
+
+// Quando o usuário clica no botão, exibe o modal
+button.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Quando o usuário clica no botão de fechamento, esconde o modal
+span.onclick = function() {
+  modal
+}
